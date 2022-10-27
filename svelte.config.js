e2f9ea5fed29ecd45ff0767e3b5ad6b416572a02
iandoesallthethings <iandoesallthethings@gmail.com>
2022-10-26 20:33:24 -0400

    Fix some editor errors.

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,21 @@
 import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
+import path from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess({
-		postcss: true
+		postcss: true,
 	}),
 
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		alias: {
+			$src: path.resolve('./src'),
+		},
+	},
 }
 
 export default config
