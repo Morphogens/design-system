a0ce13b90c55b6c67fa77e44878bb4055a7b5a64
iandoesallthethings <iandoesallthethings@gmail.com>
2022-10-26 20:36:22 -0400

    Add more examples to the example page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,63 @@
 <script lang="ts">
-	import Title from '$lib/Title.v1.svelte'
-	import Switch from '$lib/Switch.v1.svelte'
-	import tooltip from '$lib/tooltip'
+import type { Writable } from 'svelte/store'
+import Title from '$lib/Title.v1.svelte'
+import Switch from '$lib/Switch.v1.svelte'
+import tooltip from '$lib/tooltip'
+import Button from '$lib/Button.v1.svelte'
+import FlatToFlat from '$src/lib/connectors/FlatToFlat.v1.svelte'
 
-	let schedule: string
+let schedule: Writable<string> & { toggle: () => void }
 </script>
 
-<Title size="l">Hello, world</Title>
+<div class="flex flex-wrap w-full overflow-x-scroll gap-4">
+	<div class="component-type">
+		<Title size="s">Titles</Title>
 
-<Switch
-	values={['yearly', 'monthly']}
-	bind:value={schedule}
-	showLabels="both"
-	classes="border-pill"
-/>
+		<Title size="xxl">Hello, xxl</Title>
+		<Title size="xl">Hello, xl</Title>
+		<Title size="l">Hello, l</Title>
+		<Title size="m">Hello, m</Title>
+		<Title size="s">Hello, s</Title>
+	</div>
 
-<button use:tooltip={{ placement: 'top' }} title="Testing." class="bg-hover-active-orange"
-	>testing</button
->
+	<div class="component-type">
+		<Title size="s">Buttons</Title>
+		<Button size="l">Large Button</Button>
+		<Button size="m">Medium Button</Button>
+		<Button size="s">small Button</Button>
+	</div>
+
+	<div class="component-type">
+		<Title size="s">Switch</Title>
+		<Switch
+			values={['yearly', 'monthly']}
+			bind:value={schedule}
+			showLabels="both"
+			classes="border-pill"
+		/>
+	</div>
+
+	<div class="component-type">
+		<Title size="s">Connectors</Title>
+
+		<div class="flex flex-col items-center">
+			<Title size="m">Flat</Title>
+			<FlatToFlat />
+			<Title size="m">Flat</Title>
+		</div>
+	</div>
+
+	<div class="component-type">
+		<Title size="s">Tooltips</Title>
+		<button use:tooltip={{ placement: 'top' }} title="Testing." class="bg-hover-active-orange">
+			testing
+		</button>
+		<Button classes="!bg-hover-active-blue">Button</Button>
+	</div>
+</div>
+
+<style>
+.component-type {
+	@apply flex flex-col gap-4 p-4 border rounded-3xl;
+}
+</style>
